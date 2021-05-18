7b1d65b496032e6da4f1979e8519a9262ed59824
harrypooter3 <ezequielnavone.en@gmail.com>
2021-05-18 10:20:01 -0300

    Modificacion ligera para subir
    El archivo me estaba tirando error, ya que no esta completo le agregue pass para ignorarlo.

--- tp4-ej8.py
+++ tp4-ej8.py
@@ -6,12 +6,15 @@
 
 def ordenar_mayor_a_menor(uno, dos, tres):
     lista = list()
+    pass
 def ordenar_menor_a_mayor(uno, dos, tres):
+    pass
         
 def prueba():
     uno = (int(input("Ingresar el primero numero")))
     dos = (int(input("Ingresar el primero numero")))
     tres = (int(input("Ingresar el primero numero")))
+    pass
 
 
 if __name__ == "__main__":
