3f921ecca6f4efeaf2fa79e318e282fe5334d609
harrypooter3 <ezequielnavone.en@gmail.com>
2021-05-19 10:18:53 -0300

    Modificacion al ejercicio 2
    una pequeña modificación en base a comentarios del profe

--- tp4-ej2.py
+++ tp4-ej2.py
@@ -4,21 +4,21 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-def suma_lenta(numero_1, numero_2):
-    temporal = numero_1
-    resultado = numero_1 + numero_2
+def suma_lenta(numero, otro_numero):
+    temporal = numero
+    resultado = numero + otro_numero
     while temporal != resultado:
         try:
             print(f"{temporal}")
             temporal = temporal + 1
         except:
             break
-    return(f"La suma entre el numero {numero_1} y {numero_2} da como resultado {temporal}")
+    return(f"La suma entre el numero {numero} y {otro_numero} da como resultado {temporal}")
 
 def prueba():
-    numero_1 = (int(input("ingrese un numero entero: ")))
-    numero_2 = (int(input("ingrese un segundo numero entero: ")))
-    print(suma_lenta(numero_1, numero_2))
+    numero = (int(input("ingrese un numero entero: ")))
+    otro_numero = (int(input("ingrese un segundo numero entero: ")))
+    print(suma_lenta(numero, otro_numero))
 
 
 if __name__ == "__main__":
