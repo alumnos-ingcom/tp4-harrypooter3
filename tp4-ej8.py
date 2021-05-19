57bb9ff0bb6ca02b5f5cdf8df840a920e373b954
harrypooter3 <ezequielnavone.en@gmail.com>
2021-05-19 20:21:56 -0300

    Actualización ej8
    Terminado el ejercicio 8

--- tp4-ej8.py
+++ tp4-ej8.py
@@ -6,15 +6,38 @@
 
 def ordenar_mayor_a_menor(uno, dos, tres):
     lista = list()
-    pass
+    lista.append(uno)
+    lista.append(dos)
+    lista.append(tres)
+    for i in range (1,len(lista)):
+        for j in range (len(lista) - i):
+            if lista[j] < lista[j + 1]:
+                temporal = lista[j]
+                lista[j] = lista[j + 1]
+                lista[j + 1] = temporal
+    return lista
+                
 def ordenar_menor_a_mayor(uno, dos, tres):
-    pass
-        
+    lista = list()
+    lista.append(uno)
+    lista.append(dos)
+    lista.append(tres)
+    for i in range (1,len(lista)):
+        for j in range (len(lista) - i):
+            if lista[j] > lista[j + 1]:
+                temporal = lista[j]
+                lista[j] = lista[j + 1]
+                lista[j + 1] = temporal
+    return lista
+
 def prueba():
-    uno = (int(input("Ingresar el primero numero")))
-    dos = (int(input("Ingresar el primero numero")))
-    tres = (int(input("Ingresar el primero numero")))
-    pass
+    uno = (int(input("Ingresar el primer numero: ")))
+    dos = (int(input("Ingresar el segundo numero: ")))
+    tres = (int(input("Ingresar el tercer numero: ")))
+    lista = ordenar_mayor_a_menor(uno, dos, tres)
+    print(f"Lista ordenada de mayor a menor {ordenar_mayor_a_menor(uno, dos, tres)}")
+    print(f"Lista ordenada de menor a mayor {ordenar_menor_a_mayor(uno, dos, tres)}")
+    
 
 
 if __name__ == "__main__":
